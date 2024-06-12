b0a64e5fd3af7cb181d820aaa152082d5f8c154e
Junim <dbaudwns20@gmail.com>
2024-06-12 16:13:58 +0900

    메시지 컴포넌트 제거 시 다른 메시지도 같이 제거되는 버그 수정

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,10 +1,18 @@
+/**
+ * 메시지 출력 유틸
+ * - Notification, Confirm
+ * - 각 종류별 메시지는 1개만 존재할 수 있다.
+ */
+
 import { createElement } from "react";
 import { Root, createRoot } from "react-dom/client";
 
 import Notification from "@/components/message/notification/notification";
 import Confirm from "@/components/message/confirm/confirm";
 
-let messageBox: Root | null;
+import { generateRandomText } from "@/utils/common";
+
+const messageMap: Map<string, Root> = new Map<string, Root>();
 
 type NotificationOptions = {
   message: string;
@@ -25,28 +33,69 @@ type ConfirmOptions = {
   buttons: FuncButton[];
 };
 
-function setMessageBox() {
-  if (messageBox) clearMessageBox();
-  messageBox = createRoot(document.getElementById("message-box")!);
+type MessageType = "notification" | "confirm";
+
+function removeExistingMessage(messageType: MessageType) {
+  const target: string =
+    messageType === "notification" ? "notification" : "confirm";
+  Array.from(messageMap.keys()).forEach((key) => {
+    if (key.startsWith(target)) {
+      clearMessageBox(key);
+    }
+  });
 }
 
-export function clearMessageBox() {
-  if (!messageBox) return;
-  messageBox!.unmount();
+function setMessageBox(messageRootId: string): HTMLDivElement {
+  const messageBox: HTMLDivElement = document.createElement("div");
+  messageBox.setAttribute("id", messageRootId);
+  document.getElementById("message-box")!.appendChild(messageBox);
+  return messageBox;
+}
+
+export function clearMessageBox(messageRootId: string) {
+  const messageRoot: Root | undefined = messageMap.get(messageRootId);
+  if (messageRoot) {
+    // 해당 메시지 루트 언마운트
+    messageRoot.unmount();
+    // 메시지 id 로 생성된 div 삭제
+    document.getElementById(messageRootId)?.remove();
+    // Map 에서 삭제
+    messageMap.delete(messageRootId);
+  }
 }
 
 export function showNotificationMessage(options: NotificationOptions) {
-  // create root
-  setMessageBox();
-
-  // component render
-  messageBox!.render(createElement(Notification, options));
+  // 같은 유형의 메시지가 있다면 제거
+  removeExistingMessage("notification");
+  // id 정의
+  const messageRootId: string = `notification_${generateRandomText()}`;
+  // root 생성
+  const root: Root = createRoot(setMessageBox(messageRootId));
+  // 컴포넌트 렌더링
+  root.render(
+    createElement(Notification, {
+      ...options,
+      ...{ messageRootId },
+    })
+  );
+  // Map 에 저장
+  messageMap.set(messageRootId, root);
 }
 
 export async function showConfirmMessage(options: ConfirmOptions) {
-  // create root
-  setMessageBox();
-
-  // component render
-  messageBox!.render(createElement(Confirm, options));
+  // 같은 유형의 메시지가 있다면 제거
+  removeExistingMessage("confirm");
+  // id
+  const messageRootId: string = `confirm_${generateRandomText()}`;
+  // root 생성
+  const root: Root = createRoot(setMessageBox(messageRootId));
+  // 컴포넌트 렌더링
+  root.render(
+    createElement(Confirm, {
+      ...options,
+      ...{ messageRootId },
+    })
+  );
+  // Map 에 저장
+  messageMap.set(messageRootId, root);
 }
