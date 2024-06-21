e8ab69a80aa6765e8431684de9b551da40316533
Junim <dbaudwns20@gmail.com>
2024-06-21 19:25:56 +0900

    다크모드 선택 ui 추가, tailwind 설정 변경

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Image from "next/image";
+
+import useThemeMode, { type ThemeMode } from "@/hooks/useThemeMode";
 
 import "./style.css";
 
@@ -7,24 +10,76 @@ import Github from "@/assets/github.svg";
 import User from "@/assets/user.svg";
 
 export default function Footer() {
-  const { CREATOR_EMAIL, GITHUB_REPO_LINK, GITHUB_LINK } = process.env;
+  // values
+  const [theme, setTheme] = useThemeMode();
 
   return (
     <footer className="main-footer">
       <nav className="navbar">
+        <div className="theme-mode-wrapper">
+          <div className="theme is-light">
+            <input
+              id="light"
+              type="radio"
+              value="LIGHT"
+              checked={theme === "LIGHT"}
+              onChange={(e: any) => {
+                setTheme(e.target.value as ThemeMode);
+              }}
+            />
+            <label htmlFor="light">
+              <span className="icon">
+                <i className="material-icons-outlined md-18">light_mode</i>
+              </span>
+            </label>
+          </div>
+          <div className="theme is-os">
+            <input
+              id="os"
+              type="radio"
+              value="OS"
+              checked={theme === "OS"}
+              onChange={(e: any) => {
+                setTheme(e.target.value as ThemeMode);
+              }}
+            />
+            <label htmlFor="os">
+              <span className="icon">
+                <i className="material-icons-outlined md-18">desktop_windows</i>
+              </span>
+            </label>
+          </div>
+          <div className="theme is-dark">
+            <input
+              id="dark"
+              type="radio"
+              value="DARK"
+              checked={theme === "DARK"}
+              onChange={(e: any) => {
+                setTheme(e.target.value as ThemeMode);
+              }}
+            />
+            <label htmlFor="dark">
+              <span className="icon">
+                <i className="material-icons-outlined md-18">dark_mode</i>
+              </span>
+            </label>
+          </div>
+        </div>
         <div className="links">
-          <a href={`mailto:${CREATOR_EMAIL}`}>
+          <span className="text-xs fill-gray-300">Created By Junim</span>
+          <a href="mailto:dbaudwns20@gmail.com">
             <Image src={Email} alt="Email" width={20} height={20} priority />
           </a>
-          <a href={GITHUB_REPO_LINK} target="_blank">
+          <a
+            href="https://github.com/dbaudwns20/WTS-Web-Editor"
+            target="_blank"
+          >
             <Image src={Github} alt="Github" width={20} height={20} priority />
           </a>
-          <a href={GITHUB_LINK} target="_blank">
+          <a href="https://github.com/dbaudwns20" target="_blank">
             <Image src={User} alt="User" width={20} height={20} priority />
           </a>
-        </div>
-        <div>
-          <span className="text-xs fill-gray-300">Created By Junim</span>
         </div>
       </nav>
     </footer>
